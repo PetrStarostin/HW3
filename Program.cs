2a79460b5d50ecba8dd52ff3e2dae18897e8e6ae
Petr Starostin <starostin.petr@yandex.ru>
2023-03-19 20:12:33 +0400

    First task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsPolindrome(int num)
+{
+    int temp = num ; 
+    int reverb = 0 ; 
+    while(num > 0)
+    {
+        int grab = num % 10;
+        reverb = reverb * 10 + grab ; 
+        num = num / 10 ;
+    }
+    if(temp == reverb) return true ; 
+    else return false ; 
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(IsPolindrome(number))
+    Console.WriteLine($"Число {number} полиндром");
+else 
+    Console.WriteLine($"Число {number} не полиндром");
